b59a3ac7a4386ac91bab63c8f35fae7224f28d0f
Dmitry Granovsky <dima.granovsky@gmail.com>
2010-07-03 10:34:16 +0000

    messages moved to a detached config

--- lib/config.default.php
+++ lib/config.default.php
@@ -5,6 +5,7 @@ it should be written manually on checkout. You should
 copy this template as config.php and change the appropriate
 values. You should NOT svn add it.
 */
+require_once('config_msg.php');
 
 $config['web_prefix'] = ''; # '' or '/dir'
 
@@ -12,10 +13,4 @@ $config['mysql_host']   = 'localhost';
 $config['mysql_dbname'] = '';
 $config['mysql_user']   = '';
 $config['mysql_passwd'] = '';
-
-#message text
-$config['msg_loginerror'] = "Пользователь с указанной комбинацией логина и пароля не найден. Попробуйте, пожалуйста, <a href='?'>ещё раз</a>.";
-
-$config['msg_notadmin'] = 'У вас недостаточно прав для просмотра этой страницы. Возможно, вам необходимо <a href="login.php">войти</a> под своим именем и паролем.';
-$config['msg_notlogged'] = 'Для просмотра этой страницы необходимо <a href="login.php">войти</a> под своим именем и паролем.';
 ?>
