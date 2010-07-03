fc05484727bbc8a991d5757342efd249fdc2b6f9
Dmitry Granovsky <dima.granovsky@gmail.com>
2010-07-03 10:27:40 +0000

    config file is no more on svn

--- lib/config.default.php
+++ lib/config.default.php
@@ -1,10 +1,17 @@
 <?php
-$config['web_prefix'] = '/corpus'; # '' or '/dir'
+/*
+Since this configuration file is environment-dependent,
+it should be written manually on checkout. You should
+copy this template as config.php and change the appropriate
+values. You should NOT svn add it.
+*/
+
+$config['web_prefix'] = ''; # '' or '/dir'
 
 $config['mysql_host']   = 'localhost';
-$config['mysql_dbname'] = 'corpora';
-$config['mysql_user']   = 'corpora';
-$config['mysql_passwd'] = 'corpora';
+$config['mysql_dbname'] = '';
+$config['mysql_user']   = '';
+$config['mysql_passwd'] = '';
 
 #message text
 $config['msg_loginerror'] = "Пользователь с указанной комбинацией логина и пароля не найден. Попробуйте, пожалуйста, <a href='?'>ещё раз</a>.";
