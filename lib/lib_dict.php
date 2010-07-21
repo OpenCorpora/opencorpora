c9cd972da9bc16b9be9927e25d2a7abe529998e0
Dmitry Granovsky <dima.granovsky@gmail.com>
2010-07-21 16:25:34 +0000

    issue 22

--- lib/lib_dict.php
+++ lib/lib_dict.php
@@ -132,6 +132,8 @@ function addtext_add($text, $book_id, $par_num) {
             if (!sql_query("INSERT INTO `sentences` VALUES(NULL, '$par_id', '".($sent_num++)."', '0')")) return 0;
             $sent_id = sql_insert_id();
             $token_num = 1;
+            //strip excess whitespace
+            $sent = preg_replace('/\s\s+/', ' ', $sent);
             $tokens = explode(' ', $sent);
             foreach ($tokens as $token) {
                 //adding a textform
