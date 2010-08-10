f9a2f99878364cc3b2bd43be2a8d092a4b2b1594
Dmitry Granovsky <dima.granovsky@gmail.com>
2010-08-10 12:07:04 +0000

    2 useful db-dropping scripts added

---
+++ scripts/drop_dict.sh
@@ -0,0 +1,1 @@
+mysql -uroot -Dcorpora -e 'DROP TABLE dict_revisions; DROP TABLE dict_lemmata; DROP TABLE rev_sets; DROP TABLE form2lemma; \. ~/corpus/install/install.sql'
---
+++ scripts/drop_sent.sh
@@ -0,0 +1,1 @@
+mysql -uroot -Dcorpora -e 'DROP TABLE tf_revisions; DROP TABLE text_forms; DROP TABLE sentences; DROP TABLE paragraphs; \. ~/corpus/install/install.sql'
