44851e31dfcb35fb8bbb41808ed8446c19a3d3e4
Dmitry Granovsky <dima.granovsky@gmail.com>
2010-10-19 16:18:26 +0000

    no dictionary export unless it has been updated (fixes issue 60)

--- export/dict/export_dict.pl
+++ export/dict/export_dict.pl
@@ -22,8 +22,20 @@ print $lock 'lock';
 close $lock;
 
 #main
-my $dbh = DBI->connect('DBI:mysql:'.$mysql{'dbname'}.':'.$mysql{'host'}, $mysql{'user'}, $mysql{'passwd'}) or die $DBI::errstr;
+my $dbh = DBI->connect('DBI:mysql:'.$mysql{'dbname'}.':'.$mysql{'host'}, $mysql{'user'}, $mysql{'passwd'});
+if (!$dbh) {
+    unlink $lock_path;
+    die $DBI::errstr;
+}
 $dbh->do("SET NAMES utf8");
+
+my $ts = $dbh->prepare("SELECT MAX(`timestamp`) `timestamp` FROM `rev_sets` WHERE `set_id` IN ((SELECT `set_id` FROM dict_revisions ORDER BY `rev_id` DESC LIMIT 1), (SELECT `set_id` FROM dict_links_revisions ORDER BY `rev_id` DESC LIMIT 1))");
+$ts->execute();
+my $r = $ts->fetchrow_hashref();
+if (time() - $r->{'timestamp'} > 60*60*25) {
+    unlink $lock_path;
+    die ("Dictionary not updated for 25 hours, exiting");
+}
 
 my $rev = $dbh->prepare("SELECT MAX(rev_id) AS m FROM dict_revisions");
 my $read_gg = $dbh->prepare("SELECT * FROM gram_types ORDER by `orderby`");
@@ -32,8 +44,8 @@ my $read_l = $dbh->prepare("SELECT * FROM (SELECT lemma_id, rev_id, rev_text FRO
 my $read_lt = $dbh->prepare("SELECT * FROM dict_links_types ORDER BY link_id");
 my $read_links = $dbh->prepare("SELECT * FROM dict_links ORDER BY link_id LIMIT ?, 10000");
 
-$rev->execute() or die $DBI::errstr;
-my $r = $rev->fetchrow_hashref();
+$rev->execute();
+$r = $rev->fetchrow_hashref();
 my $r1;
 my $maxrev = $r->{'m'};
 
@@ -43,10 +55,10 @@ my $footer = "</dictionary>";
 # grammems
 my $grams = "<grammems>\n";
 
-$read_gg->execute() or die $DBI::errstr;
+$read_gg->execute();
 while($r = $read_gg->fetchrow_hashref()) {
     $grams .= "    <group name=\"".tidy_xml($r->{'type_name'})."\">\n";
-    $read_g->execute($r->{'type_id'}) or die $DBI::errstr;
+    $read_g->execute($r->{'type_id'});
     while($r1 = $read_g->fetchrow_hashref()) {
         $grams .= "        <grammem>".tidy_xml($r1->{'inner_id'})."</grammem>\n";
     }
@@ -64,7 +76,7 @@ my $min_lid = 0;
 
 while ($flag) {
     $flag = 0;
-    $read_l->execute($min_lid + 1, $min_lid + 50000) or die $DBI::errstr;
+    $read_l->execute($min_lid + 1, $min_lid + 50000);
     while($r = $read_l->fetchrow_hashref()) {
         $flag = 1;
         $r->{'rev_text'} =~ s/<\/?dr>//g;
@@ -93,7 +105,7 @@ $flag = 1;
 
 while($flag) {
     $flag = 0;
-    $read_links->execute($min_lid) or die $DBI::errstr;
+    $read_links->execute($min_lid);
     while($r = $read_links->fetchrow_hashref()) {
         $flag = 1;
         print '    <link id="'.$r->{'link_id'}.'" from="'.$r->{'lemma1_id'}.'" to="'.$r->{'lemma2_id'}.'" type="'.$r->{'link_type'}."\"/>\n";
