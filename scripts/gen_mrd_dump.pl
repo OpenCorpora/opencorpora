a1aa285cf9ea3022b1d2dea7dcb7ae03e9f4852b
Victor Bocharov <victor.bocharov@gmail.com>
2010-10-26 20:36:17 +0000

    some bugs fixed

--- scripts/gen_mrd_dump.pl
+++ scripts/gen_mrd_dump.pl
@@ -10,34 +10,43 @@ binmode(STDERR, ":encoding(utf-8)");
 
 for (my $l = 0; $l < $d->MaxLemmaNo(); $l++) {
   my $lemma = $d->GetLemma($l);
-  if ($lemma->GetDefForm()->Text() =~ /^([А-ЯЁа-яёA-Za-z]+)\-([А-ЯЁа-яёA-Za-z]+)$/
-      && $lemma->GetPOS() eq "П" ) {
-    #print STDERR "form with dash found: " . $lemma->GetDefForm()->Text() . " POS=" . $lemma->GetPOS() . "\n";
+  if ($lemma->GetDefForm()->Text() =~ /^([А-ЯЁа-яёA-Za-z]+)\-([А-ЯЁа-яёA-Za-z]+)$/) {
     my ($p1, $p2) = ($1, $2);
     if ($p2 !~ /[Ёё]/ && $p2 =~ /[Ее]/) {
-      while ($p2 =~ /^([А-ЯЁа-яё]*)[Ее]([А-ЯЁа-яё]*)$/g) {
-        my $w = $1 . "Ё" . $2;
-        my $i = $d->Lookup($w);
-        if (defined $i) {
-          my $related_lemma;
-          foreach my $mv (@{$i}) {
-            $related_lemma = $d->GetLemma($mv->LemmaId());
-            print STDERR "$p1-$p2 " . $lemma->ParadigmId() . " -> $w " . $related_lemma->ParadigmId() . "\n";
-            if ($lemma->GetPOS() eq $related_lemma->GetPOS()) {
+      if ($lemma->GetPOS() eq "П" ) {
+        while ($p2 =~ /^([А-ЯЁа-яё]*)[Ее]([А-ЯЁа-яё]*)$/g) {
+          my $w = $1 . "Ё" . $2;
+          my $i = $d->Lookup($w);
+          if (defined $i) {
+            my $related_lemma;
+            foreach my $mv (@{$i}) {
+              $related_lemma = $d->GetLemma($mv->LemmaId());
+              #print STDERR "$p1-$p2 " . $lemma->ParadigmId() . " -> $w " . $related_lemma->ParadigmId() . "\n";
+              if ($lemma->GetPOS() eq $related_lemma->GetPOS()) {
+                last;
+              } else {
+                $related_lemma = undef;
+              }
+            }
+            if (defined $related_lemma) {
+              $lemma->SetStem($p1 . "-" . $related_lemma->Stem());
+              $lemma->SetParadigmId($related_lemma->ParadigmId());
+              #print STDERR "SetStem from $p1-$p2 to $p1-$w\n";
               last;
-            } else {
-              $related_lemma = undef;
             }
           }
-          if (defined $related_lemma) {
-            $lemma->SetStem($p1 . "-" . $related_lemma->Stem());
-            $lemma->SetParadigmId($related_lemma->ParadigmId());
-            print STDERR "SetStem from $p1-$p2 to $p1-$w\n";
-            last;
+        } 
+      } else {
+        # не прилагательное, а другая часть речи
+        while ($p2 =~ /^([А-ЯЁа-яё]*)[Ее]([А-ЯЁа-яё]*)$/g) {
+          my $w = $1 . "Ё" . $2;
+          my $i = $d->Lookup($w);
+          if (defined $i) {
+            #print STDERR "$p1-$p2 -> $p1-$w\n";
           }
-        }
+        } 
       }
-    }
+    } # если во второй половине слова нет Ё, но есть Е  
   }
 
   print "PARA " . $lemma->ParadigmId() . "\n";
