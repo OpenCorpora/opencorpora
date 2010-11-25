b0fbe00bb061f914c9c7d8b94036ece4704fdbe9
Dmitry Granovsky <dima.granovsky@gmail.com>
2010-11-25 19:33:12 +0000

    dict export fixed (fixes issue 109)

--- export/dict/export_dict.pl
+++ export/dict/export_dict.pl
@@ -39,8 +39,7 @@ if (time() - $r->{'timestamp'} > 60*60*25 && !FORCE) {
 }
 
 my $rev = $dbh->prepare("SELECT MAX(rev_id) AS m FROM dict_revisions");
-my $read_gg = $dbh->prepare("SELECT * FROM gram_types ORDER by `orderby`");
-my $read_g = $dbh->prepare("SELECT inner_id FROM gram WHERE gram_type=? ORDER BY `orderby`");
+my $read_g = $dbh->prepare("SELECT g1.inner_id AS id, g2.inner_id AS pid FROM gram g1 LEFT JOIN gram g2 ON (g1.parent_id=g2.gram_id) ORDER BY g1.`orderby`");
 my $read_l = $dbh->prepare("SELECT * FROM (SELECT lemma_id, rev_id, rev_text FROM dict_revisions WHERE lemma_id BETWEEN ? AND ? ORDER BY lemma_id, rev_id DESC) T GROUP BY T.lemma_id");
 my $read_lt = $dbh->prepare("SELECT * FROM dict_links_types ORDER BY link_id");
 my $read_links = $dbh->prepare("SELECT * FROM dict_links ORDER BY link_id LIMIT ?, 10000");
@@ -50,20 +49,15 @@ $r = $rev->fetchrow_hashref();
 my $r1;
 my $maxrev = $r->{'m'};
 
-my $header = "<?xml version=\"1.0\" encoding=\"utf8\" standalone=\"yes\"?>\n<dictionary version=\"0.7\" revision=\"$maxrev\">\n";
+my $header = "<?xml version=\"1.0\" encoding=\"utf8\" standalone=\"yes\"?>\n<dictionary version=\"0.8\" revision=\"$maxrev\">\n";
 my $footer = "</dictionary>";
 
 # grammems
 my $grams = "<grammems>\n";
 
-$read_gg->execute();
-while($r = $read_gg->fetchrow_hashref()) {
-    $grams .= "    <group name=\"".tidy_xml($r->{'type_name'})."\">\n";
-    $read_g->execute($r->{'type_id'});
-    while($r1 = $read_g->fetchrow_hashref()) {
-        $grams .= "        <grammem>".tidy_xml($r1->{'inner_id'})."</grammem>\n";
-    }
-    $grams .= "    </group>\n";
+$read_g->execute();
+while($r = $read_g->fetchrow_hashref()) {
+    $grams .= "    <grammem parent=\"$r->{'pid'}\">".tidy_xml($r->{'id'})."</grammem>\n";
 }
 $grams .= "</grammems>\n";
 
