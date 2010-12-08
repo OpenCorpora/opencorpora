bfb6ca850d4902c40fe490845038ffbd1278ee2e
Dmitry Granovsky <dima.granovsky@gmail.com>
2010-12-08 08:56:50 +0000

    semi-stub for dict checker

---
+++ scripts/dict_check.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl
+use strict;
+use utf8;
+use DBI;
+use Encode;
+
+my $lock_path = "/var/lock/opcorpora_dictcheck.lock";
+if (-f $lock_path) {
+    die ("lock exists, exiting");
+}
+
+#reading config
+my %mysql;
+while(<>) {
+    if (/\$config\['mysql_(\w+)'\]\s*=\s*'([^']+)'/) {
+        $mysql{$1} = $2;
+    }
+}
+
+#open my $lock, ">$lock_path";
+#print $lock 'lock';
+#close $lock;
+
+#main
+my %bad_pairs;
+
+my $dbh = DBI->connect('DBI:mysql:'.$mysql{'dbname'}.':'.$mysql{'host'}, $mysql{'user'}, $mysql{'passwd'}) or die $DBI::errstr;
+$dbh->do("SET NAMES utf8");
+my $clear = $dbh->prepare("DELETE FROM dict_errata WHERE rev_id IN (SELECT rev_id FROM dict_revisions WHERE lemma_id=?)");
+my $update = $dbh->prepare("UPDATE dict_revisions SET dict_check='1' WHERE rev_id=? LIMIT 1");
+
+get_gram_info();
+my @revisions = @{get_new_revisions()};
+while(my $ref = shift @revisions) {
+    $clear->execute($ref->{'lemma_id'});
+    check($ref) && $update->execute($ref->{'id'});
+}
+
+#unlink ($lock_path);
+
+##### SUBROUTINES #####
+sub get_new_revisions {
+    my $scan = $dbh->prepare("SELECT rev_id, lemma_id, rev_text FROM dict_revisions WHERE dict_check=0 ORDER BY rev_id LIMIT 100");
+    $scan->execute();
+    my $txt;
+    my @revs;
+    while(my $ref = $scan->fetchrow_hashref()) {
+        $txt = decode('utf8', $ref->{'rev_text'});
+        push @revs, {'id' => $ref->{'rev_id'}, 'lemma_id' => $ref->{'lemma_id'}, 'text' => $txt};
+    }
+    return \@revs;
+}
+sub get_gram_info {
+    my $scan0 = $dbh->prepare("SELECT gram_id, inner_id FROM gram WHERE parent_id=0");
+    $scan0->execute();
+    my %h;
+    while(my $ref = $scan0->fetchrow_hashref()) {
+        %h = ();
+        $h{$ref->{'inner_id'}} = 0;
+        my $scan1 = $dbh->prepare("SELECT gram_id, inner_id FROM gram WHERE parent_id=".$ref->{'gram_id'});
+        $scan1->execute();
+        while(my $ref1 = $scan1->fetchrow_hashref()) {
+            $h{$ref1->{'inner_id'}} = 0;
+            my $scan2 = $dbh->prepare("SELECT gram_id, inner_id FROM gram WHERE parent_id=".$ref1->{'gram_id'});
+            $scan2->execute();
+            while (my $ref2 = $scan2->fetchrow_hashref()) {
+                $h{$ref2->{'inner_id'}} = 0;
+            }
+        }
+        if (scalar keys %h > 1) {
+            #this is a cluster
+            for my $k1(keys %h) {
+                for my $k2(keys %h) {
+                    next if $k1 eq $k2;
+                    $bad_pairs{"$k1|$k2"} = $bad_pairs{"$k2|$k1"} = 0;
+                }
+            }
+        }
+    }
+}
+sub check {
+    my $ref = shift;
+    0;
+}
