05afbef57a19fba99a2ceb7e5346e45b9afd071f
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-02-13 13:17:55 +0000

    forgotten script for r441 (fixes issue 65)

---
+++ scripts/update_stats.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+use strict;
+use utf8;
+use DBI;
+use Encode;
+
+my $lock_path = "/var/lock/opcorpora_updstats.lock";
+if (-f $lock_path) {
+    die ("lock exists, exiting");
+}
+
+#reading config
+my %mysql;
+while(<>) {
+    if (/\$config\['mysql_(\w+)'\]\s*=\s*'([^']+)'/) {
+        $mysql{$1} = $2;
+    }
+}
+
+open my $lock, ">$lock_path";
+print $lock 'lock';
+close $lock;
+
+my $dbh = DBI->connect('DBI:mysql:'.$mysql{'dbname'}.':'.$mysql{'host'}, $mysql{'user'}, $mysql{'passwd'}) or die $DBI::errstr;
+$dbh->do("SET NAMES utf8");
+my $scan = $dbh->prepare("SELECT * FROM `stats_param` WHERE is_active=1 ORDER BY param_id");
+my $insert = $dbh->prepare("INSERT INTO `stats_values` VALUES(?, ?, ?)");
+
+$scan->execute();
+
+# SUBROUTINES
+
+my $func;
+
+$func->{'total_books'} = sub {
+    my $sc = $dbh->prepare("SELECT COUNT(*) AS cnt FROM books");
+    $sc->execute();
+    return $sc->fetchrow_hashref()->{'cnt'};
+};
+$func->{'total_sentences'} = sub {
+    my $sc = $dbh->prepare("SELECT COUNT(*) AS cnt FROM sentences");
+    $sc->execute();
+    return $sc->fetchrow_hashref()->{'cnt'};
+};
+$func->{'total_tokens'} = sub {
+    my $sc = $dbh->prepare("SELECT COUNT(*) AS cnt FROM text_forms");
+    $sc->execute();
+    return $sc->fetchrow_hashref()->{'cnt'};
+};
+$func->{'total_lemmata'} = sub {
+    my $sc = $dbh->prepare("SELECT COUNT(*) AS cnt FROM dict_lemmata");
+    $sc->execute();
+    return $sc->fetchrow_hashref()->{'cnt'};
+};
+
+# /SUBROUTINES
+
+my $value;
+while (my $ref = $scan->fetchrow_hashref()) {
+    if (exists $func->{$ref->{'param_name'}}) {
+        $value = $func->{$ref->{'param_name'}}->();
+        $insert->execute(time(), $ref->{'param_id'}, $value);
+    }
+}
+
+unlink ($lock_path);
