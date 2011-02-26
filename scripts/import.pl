f76ea0e839ea9ddc7c7b43cc2d286e70975d31c5
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-02-26 12:25:12 +0000

    addition to import rules (fixing duplicate gent+plur in some adjectives)

--- scripts/import.pl
+++ scripts/import.pl
@@ -13,4 +13,5 @@ my $importer = new OpenCorpora::Dict::Importer;
 $importer->read_rules('/home/grand/corpus/scripts/import_rules.txt');
 $importer->read_bad_lemma_grammems('/home/grand/corpus/scripts/bad_lemma_grammems.txt');
 $importer->preload_list('anim0', '/home/grand/corpus/scripts/lists/Del_anim-inan&Add_ANim.txt');
+$importer->preload_list('numr0', '/home/grand/corpus/scripts/lists/list_numr_dupl_gent.txt');
 $importer->read_aot('/home/grand/aot_dump.2');
