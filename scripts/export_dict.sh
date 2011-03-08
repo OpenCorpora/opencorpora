2a24215b08c984602ac7b85e8d6aede5d78cd3c7
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-03-08 17:36:52 +0000

    added zipping to dict export (issue 143)

--- scripts/export_dict.sh
+++ scripts/export_dict.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 touch /var/lock/oc_readonly.lock
-newpath=/corpus/files/export/dict/dict.opcorpora.xml.bz2.new
-/corpus/export/dict/export_dict.pl $1 </corpus/lib/config.php | bzip2 -c9 >$newpath
+newpath=/corpus/files/export/dict/dict.opcorpora.xml
+/corpus/export/dict/export_dict.pl $1 </corpus/lib/config.php >$newpath
 if [ `ls -l $newpath | awk '{print $5}'` -gt 100 ]; then
-    mv $newpath /corpus/files/export/dict/dict.opcorpora.xml.bz2
+    bzip2 -c9 $newpath >$newpath.bz2.new
+    mv $newpath.bz2.new $newpath.bz2
+    zip -9 $newpath.zip.new $newpath
+    mv $newpath.zip.new $newpath.zip
+    rm $newpath
 else
     echo 'Generated dictionary export is too small, no overwriting' >&2
 fi
