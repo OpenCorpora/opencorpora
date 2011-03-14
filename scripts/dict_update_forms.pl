6f959fc2a4b97901edb40468d27b5e798b04ad23
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-03-14 13:37:18 +0000

    dict_update fix (fixes issue 146)

--- scripts/dict_update_forms.pl
+++ scripts/dict_update_forms.pl
@@ -24,6 +24,14 @@ close $lock;
 my $dbh = DBI->connect('DBI:mysql:'.$mysql{'dbname'}.':'.$mysql{'host'}, $mysql{'user'}, $mysql{'passwd'}) or die $DBI::errstr;
 $dbh->do("SET NAMES utf8");
 
+#if there are any words still not checked by form2tf, we should do nothing
+my $prescan = $dbh->prepare("SELECT tf_id, tf_text FROM text_forms WHERE tf_id NOT IN (SELECT tf_id FROM form2tf) LIMIT 1");
+$prescan->execute();
+if($prescan->fetchrow_hashref()) {
+    unlink ($lock_path);
+    die "form2tf isn't up to date";
+}
+
 my $scan = $dbh->prepare("SELECT form_text FROM updated_forms LIMIT ?");
 my $scan_f2tf = $dbh->prepare("SELECT tf_id FROM form2tf WHERE form_text=?");
 my $del = $dbh->prepare("DELETE FROM updated_forms WHERE form_text=?");
