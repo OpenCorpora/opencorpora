f824c7b57f2b1f64cd9e5fc7c0cac8e267f04cda
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-03-25 09:45:38 +0000

    bugfix in text adding (broken in r512)

--- ajax/select_book.php
+++ ajax/select_book.php
@@ -3,6 +3,10 @@ header('Content-type: text/xml; charset=utf-8');
 require_once('../lib/header.php');
 require_once('../lib/lib_books.php');
 $id = (int)$_GET['id'];
-$out = books_get_select($id);
+$t = get_books_for_select($id);
+$out = '';
+foreach($t as $id=>$title) {
+    $out .= "<option value='$id'>$title</option>";
+}
 echo '<?xml version="1.0" encoding="utf-8" standalone="yes"?><response>'.$out.'</response>';
 ?>
