7019c3c3836901ee8d9e575930a337e556f0c97c
Maria Nikolaeva <mary.nikolaeva@gmail.com>
2011-04-05 14:05:24 +0000

    tiny bug fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    
+    $(".submit_link").click(function(event){
+        $(this).closest('form').submit()
+        })
+    
+    })
+
 function byid(id) {
     return document.getElementById(id)
 }
@@ -11,9 +19,6 @@ function toggle(el) {
     if (el.style.display == 'none') el.style.display='block';
         else el.style.display='none';
 }
-$(".submit_link").click(function(event){
-    $(this).closest('form').submit()
-    })
 function makeRequest() {
     var req = false;
     if (window.XMLHttpRequest) {
