4b2f10a45e463869b18710a9600d82ada430d344
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-04-27 12:45:24 +0000

    UTC->MSK (downloads page)

--- lib/common.php
+++ lib/common.php
@@ -80,13 +80,13 @@ function get_downloads_info() {
     $dict = array();
     $stat = stat('files/export/dict/dict.opcorpora.xml.bz2');
     $dict['xml']['bz2']['size'] = sprintf("%.2f", $stat[7] / (1024 * 1024));
-    $dict['xml']['updated'] = date('d.m.Y H:i \U\T\C', $stat[9]);
+    $dict['xml']['updated'] = date('d.m.Y H:i \M\S\K', $stat[9]);
     $stat = stat('files/export/dict/dict.opcorpora.xml.zip');
     $dict['xml']['zip']['size'] = sprintf("%.2f", $stat[7] / (1024 * 1024));
 
     $stat = stat('files/export/dict/dict.opcorpora.txt.bz2');
     $dict['txt']['bz2']['size'] = sprintf("%.2f", $stat[7] / (1024 * 1024));
-    $dict['txt']['updated'] = date('d.m.Y H:i \U\T\C', $stat[9]);
+    $dict['txt']['updated'] = date('d.m.Y H:i \M\S\K', $stat[9]);
     $stat = stat('files/export/dict/dict.opcorpora.txt.zip');
     $dict['txt']['zip']['size'] = sprintf("%.2f", $stat[7] / (1024 * 1024));
 
