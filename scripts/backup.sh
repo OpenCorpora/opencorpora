22a979bfc5856631256cf58a069a8256fd7c572c
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-05-17 07:16:43 +0000

    ignoring some tables upon backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,5 +3,9 @@ touch /var/lock/oc_readonly.lock
 if [ ! -d /data/backup/`date +%Y%m` ]; then
 	mkdir /data/backup/`date +%Y%m`
 fi
-mysqldump opcorpora | bzip2 -c9 > /data/backup/`date +%Y%m`/oc`date +%Y%m%d_%H%M`.sql.bz2
+mysqldump \
+    --ignore-table=opcorpora.dict_errata \
+    --ignore-table=opcorpora.form2lemma \
+    --ignore-table=opcorpora.form2tf \
+    opcorpora | bzip2 -c9 > /data/backup/`date +%Y%m`/oc`date +%Y%m%d_%H%M`.sql.bz2
 rm /var/lock/oc_readonly.lock
