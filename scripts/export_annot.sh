2172a886f21b981aef1c3b7e358763a4a7c81274
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-05-19 14:48:55 +0000

    added bash script for annotation export

---
+++ scripts/export_annot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+touch /var/lock/oc_readonly.lock
+export_path=/corpus/files/export/annot/annot.opcorpora
+/corpus/export/annot/export_annot.pl /corpus/lib/config.php >$export_path.xml
+bzip2 -c9 $export_path.xml >$export_path.xml.bz2.new
+mv $export_path.xml.bz2.new $export_path.xml.bz2
+zip -9 $export_path.xml.zip.new $export_path.xml
+mv $export_path.xml.zip.new $export_path.xml.zip
+rm $export_path.xml
+rm /var/lock/oc_readonly.lock
