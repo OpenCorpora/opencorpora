77e63df83eeee5272ab43db0c4d7c285e68de03e
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-05-21 09:41:41 +0000

    less readonly mode in export_dict

--- scripts/export_dict.sh
+++ scripts/export_dict.sh
@@ -2,6 +2,7 @@
 touch /var/lock/oc_readonly.lock
 newpath=/corpus/files/export/dict/dict.opcorpora
 /corpus/export/dict/export_dict.pl $1 </corpus/lib/config.php >$newpath.xml
+rm /var/lock/oc_readonly.lock
 if [ `ls -l $newpath.xml | awk '{print $5}'` -gt 100 ]; then
     bzip2 -c9 $newpath.xml >$newpath.xml.bz2.new
     mv $newpath.xml.bz2.new $newpath.xml.bz2
@@ -9,7 +10,9 @@ if [ `ls -l $newpath.xml | awk '{print $5}'` -gt 100 ]; then
     mv $newpath.xml.zip.new $newpath.xml.zip
     rm $newpath.xml
 
+    touch /var/lock/oc_readonly.lock
     /corpus/export/dict/export_dict.pl $1 --PLAINTEXT </corpus/lib/config.php >$newpath.txt
+    rm /var/lock/oc_readonly.lock
     bzip2 -c9 $newpath.txt >$newpath.txt.bz2.new
     mv $newpath.txt.bz2.new $newpath.txt.bz2
     zip -9 $newpath.txt.zip.new $newpath.txt
@@ -18,4 +21,3 @@ if [ `ls -l $newpath.xml | awk '{print $5}'` -gt 100 ]; then
 else
     echo 'Generated dictionary export is too small, no overwriting' >&2
 fi
-rm /var/lock/oc_readonly.lock
