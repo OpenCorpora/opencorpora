57ecf249e63e95c250ee2736dcf6f8927e9140c9
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-07-27 15:23:41 +0000

    ignoring tokenizer_strange on backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,5 +7,6 @@ mysqldump \
     --ignore-table=opcorpora.dict_errata \
     --ignore-table=opcorpora.form2lemma \
     --ignore-table=opcorpora.form2tf \
+    --ignore-table=opcorpora.tokenizer_strange \
     opcorpora | bzip2 -c9 > /data/backup/`date +%Y%m`/oc`date +%Y%m%d_%H%M`.sql.bz2
 rm /var/lock/oc_readonly.lock
