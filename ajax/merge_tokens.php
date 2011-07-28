6bfa5c25b8f7dc0ba7c4c0489f99f088fd4613b6
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-07-28 11:49:02 +0000

    checking user's permission to merge tokens

--- ajax/merge_tokens.php
+++ ajax/merge_tokens.php
@@ -3,7 +3,7 @@ require_once('../lib/header.php');
 require_once('../lib/lib_books.php');
 header('Content-type: text/xml; charset=utf-8');
 echo '<?xml version="1.0" encoding="utf-8" standalone="yes"?>';
-if (!isset($_GET['ids'])) {
+if (!user_has_permission('perm_adder') || !isset($_GET['ids'])) {
     echo '<result ok="0"/>';
     return;
 }
