89af1008b0f77a1bc4c8f8ca7028a82ff029b589
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-09-08 18:05:16 +0000

    script to check remaining free space (fixes issue 272)

---
+++ scripts/check_space.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ `df | grep dat_opencorpora | awk '{print $4}'` -lt 50000 ]; then
+    touch /var/lock/oc_readonly.lock
+    echo 'Insufficient space on opencorpora.org!' | sendmail dima.granovsky@gmail.com
+fi
