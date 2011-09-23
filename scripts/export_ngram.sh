a4aa25843628beccd682ca7cc583c25e90008e3f
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-09-23 13:47:17 +0000

    bash export script for ngrams refactored

--- scripts/export_ngram.sh
+++ scripts/export_ngram.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-
 ROOT_PATH=${ROOT_PATH:-/corpus}
 
 dump_path=$ROOT_PATH/files/export/annot/annot.opcorpora.xml
 export_path=$ROOT_PATH/files/export/ngrams
 
-$ROOT_PATH/export/export_ngram.pl -f $dump_path -n 1 | bzip2 >$export_path/unigrams.bz2
-bzcat $export_path/unigrams.bz2 | head -100 >$export_path/unigrams.top100
+if [ ! -f $dump_path ]; then
+    echo "Dump not found"
+    exit 1
+fi
 
-$ROOT_PATH/export/export_ngram.pl -f $dump_path -l -n 1 | bzip2 >$export_path/unigrams.lc.bz2
-bzcat $export_path/unigrams.lc.bz2 | head -100 >$export_path/unigrams.lc.top100
+function process() {
+    if [ -z "$1" ]; then
+        echo "No argument specified"
+        return 1
+    fi
 
-$ROOT_PATH/export/export_ngram.pl -f $dump_path -n 2 | bzip2 >$export_path/bigrams.bz2
-bzcat $export_path/bigrams.bz2 | head -100 >$export_path/bigrams.top100
+    cat $1 | head -100 >$1.top100
+    bzip2 -c9 $1 > $1.bz2.new
+    mv $1.bz2.new $1.bz2
+    zip -9 $1.zip.new $1
+    mv $1.zip.new $1.zip
+    rm -f $1
+}
 
-$ROOT_PATH/export/export_ngram.pl -f $dump_path -l -n 2 | bzip2 >$export_path/bigrams.lc.bz2
-bzcat $export_path/bigrams.lc.bz2 | head -100 >$export_path/bigrams.lc.top100
+$ROOT_PATH/export/export_ngram.pl -f $dump_path -n 1 >$export_path/unigrams
+process $export_path/unigrams
 
-rm $dump_path
+$ROOT_PATH/export/export_ngram.pl -f $dump_path -l -n 1 >$export_path/unigrams.lc
+process $export_path/unigrams.lc
+
+$ROOT_PATH/export/export_ngram.pl -f $dump_path -n 2 >$export_path/bigrams
+process $export_path/bigrams
+
+$ROOT_PATH/export/export_ngram.pl -f $dump_path -l -n 2 >$export_path/bigrams.lc
+process $export_path/bigrams.lc
