44db3dfe673906e40329fe71f9e29a581c2501bd
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-10-12 15:18:26 +0000

    free blogs to add (fixes issue 283)

--- lib/lib_books.php
+++ lib/lib_books.php
@@ -437,7 +437,7 @@ function get_sources_page($skip = 0, $show_type = '', $src = 0) {
     elseif ($show_type == 'active')
         $q_tail = "WHERE s.user_id > 0 OR s.book_id > 0";
     elseif ($show_type == 'free')
-        $q_tail = "WHERE s.parent_id=$src AND s.user_id = 0";
+        $q_tail = "WHERE (s.parent_id=$src OR s.parent_id IN (SELECT source_id FROM sources WHERE parent_id=$src)) AND s.user_id = 0";
     $q_tail2 = $show_type == 'free' ? " ORDER BY RAND() LIMIT 200" : " ORDER BY s.book_id DESC, s.source_id LIMIT $skip,200";
     $r = sql_fetch_array(sql_query($q_cnt.$q_tail));
     $out['total'] = $r['cnt'];
