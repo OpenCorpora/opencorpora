917e8ff1c305daebfebf0dc090780db7630a6462
Svetlana Alexeeva <sv.bichineva@gmail.com>
2011-10-20 08:12:37 +0000

    Новые правила и списки по комментариям 12 и 22 (задача 164)

--- scripts/import.pl
+++ scripts/import.pl
@@ -29,13 +29,18 @@ $importer->preload_list('adjf_fixd_noun', "$root_path/scripts/lists/list_adjf_fi
 $importer->preload_list('nouns_subst', "$root_path/scripts/lists/nouns_subst.txt");
 $importer->preload_list('arch', "$root_path/scripts/lists/add_Arch.txt");
 $importer->preload_list('arch0', "$root_path/scripts/lists/add_Arch_nomn_plur.txt");
+$importer->preload_list('arch_adjs', "$root_path/scripts/lists/add_Arch_ADJS.txt");
+$importer->preload_list('arch_adj', "$root_path/scripts/lists/add_Arch_ADJF.txt");
+$importer->preload_list('dist_prts', "$root_path/scripts/lists/add_Dist_PRTS.txt");
 $importer->preload_list('infr0', "$root_path/scripts/lists/add_Infr_nomn_plur.txt");
 $importer->preload_list('infr1', "$root_path/scripts/lists/add_Infr_gent_plur.txt");
 $importer->preload_list('infr2', "$root_path/scripts/lists/add_Infr_VERB.txt");
 $importer->preload_list('infr3', "$root_path/scripts/lists/add_Infr_ADJS.txt");
 $importer->preload_list('pred_del', "$root_path/scripts/lists/pred_del.txt");
+$importer->preload_list('adjs_del', "$root_path/scripts/lists/adjs_forms_del.txt");
 $importer->preload_list('pred_intj', "$root_path/scripts/lists/pred_to_intj.txt");
 $importer->preload_list('erro_adjs', "$root_path/scripts/lists/add_Erro_ADJS.txt");
+$importer->preload_list('erro_prts', "$root_path/scripts/lists/add_Erro_PRTS.txt");
 $importer->preload_list('count', "$root_path/scripts/lists/add_Coun_gent_plur.txt");
 $importer->read_aot("/data/files/aot_dump.3");
 #$importer->read_aot("$root_path/scripts/test.txt");
