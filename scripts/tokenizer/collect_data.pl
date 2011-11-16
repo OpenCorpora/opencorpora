2955aa50f8848d8be1ce47ff627c3677e2e135a5
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-11-16 13:42:18 +0000

    reverse-merge r917, moved to branch

--- scripts/tokenizer/collect_data.pl
+++ scripts/tokenizer/collect_data.pl
@@ -357,8 +357,7 @@ sub looks_like_url {
     my $suffix = shift;
     return 0 if $suffix eq '';
     return 0 if $s =~ /^\./;
-    return 0 if length($s) < 5; 
-    if ($s =~ /^\W*https?\:\/\// || $s =~ /^\W*www\./ || $s =~/.\.(?:[a-z]{2,3}|ру|рф)\W*$/i) {
+    if ($s =~ /^\W*https?\:\/\// || $s =~/.\.(ru|ua|de|com|org|gov|us|ру|рф)\W*$/i) {
         return 1;
     }
     return 0;
