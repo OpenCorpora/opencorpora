38e73b3a9de26a1f1e25e3acab8e89f64dd084b1
Alexey Surikov <ksurent@gmail.com>
2012-03-03 15:56:59 +0000

    reverted previous commit because SVN is stupid =\

--- scripts/tokenizer/collect_data.pl
+++ scripts/tokenizer/collect_data.pl
@@ -5,7 +5,6 @@ use DBI;
 use Encode;
 use Unicode::Normalize;
 use Config::INI::Reader;
-use AI::DecisionTree;
 
 use constant CROSSVAL_FOLDS => 10;
 use constant THRESHOLDS => qw/0.0 0.01 0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 0.99 1.0/;
--- scripts/tokenizer/graph_metrics.pl
+++ scripts/tokenizer/graph_metrics.pl
@@ -46,7 +46,6 @@ while($threshold < 1) {
     for(1 .. $opts{jobs}) {
         my @cmd = (
             'perl',
-            "-I$ENV{HOME}/Lingua--RU--OpenCorpora--Tokenizer/blib/lib",
             $opts{script},
             "--config=$opts{config}",
             sprintf("--threshold=%.${fmt}f", $threshold),
@@ -62,7 +61,6 @@ while($threshold < 1) {
         ) or die "failed to execute '@{[ join ' ', @cmd]}'";
 
         $threshold += $opts{step};
-        last if $threshold >= 1;
     }
 
     while(my(undef, $event, $data) = watch_jobs()) {
