78613fffe06b5f6ca21a8281c076cab6ae89c2b1
Alexey Surikov <ksurent@gmail.com>
2012-04-08 19:25:29 +0000

    fixed bugs in metrics calculation

--- scripts/tokenizer/calculate_metrics.pl
+++ scripts/tokenizer/calculate_metrics.pl
@@ -8,12 +8,12 @@ use DBI;
 use Pod::Usage;
 use Getopt::Long;
 use Config::INI::Reader;
-use List::MoreUtils qw(any);
 use Lingua::RU::OpenCorpora::Tokenizer;
 
 GetOptions(
     \my %opts,
     'help',
+    'skip=s',
     'config=s',
     'data_dir=s',
     'threshold=f',
@@ -38,68 +38,90 @@ my $tokenizer = Lingua::RU::OpenCorpora::Tokenizer->new(
     (data_dir => $opts{data_dir}) x !!$opts{data_dir},
 );
 
-my $_separator = 'º';
-my $ethalons   = $dbh->selectall_arrayref("
-    select
-        source,
-        group_concat(tf_text order by text_forms.pos separator '$_separator')
-    from
-        sentences
-    join
-        text_forms
-    using
-        (sent_id)
-    group by
-        source
-");
-$_->[1] = all_occurences($_->[1], $_separator) for @$ethalons;
+my %skip;
+if(defined $opts{skip}) {
+    %skip = do {
+        open my $fh, '<', $opts{skip} or die $!;
 
-my %tokens = (
-    expected => 0,
-    good     => 0,
-    total    => 0,
-);
-
-for my $ethalon (@$ethalons) {
-    my @tokenized = map  $_->[0],
-                    grep $_->[1] >= $opts{threshold},
-                    @{ $tokenizer->tokens_bounds($ethalon->[0]) };
-
-    $tokens{total}    += @tokenized;
-    $tokens{expected} += @{ $ethalon->[1] };
-    for my $pos (@tokenized) {
-        $tokens{good}++ if any { $pos == $_ } @{ $ethalon->[1] };
+        map { chomp; ($_,undef) }
+        grep { not /^#/ and not /^\s*$/ }
+        <$fh>;
     }
 }
 
-my $precision = $tokens{good} / $tokens{total};
-my $recall    = $tokens{good} / $tokens{expected};
+my %stats = (
+    tp => 0,
+    fp => 0,
+    fn => 0,
+);
+
+my $sth = $dbh->prepare('select sent_id, source from sentences');
+$sth->execute;
+while(my($id, $text) = $sth->fetchrow_array) {
+    next if exists $skip{$id};
+
+    my $ethalon = get_bounds($text, get_tokens($id));
+    my %bounds = map  +($_->[0], undef),
+                 grep $_->[1] >= $opts{threshold},
+                 @{ $tokenizer->tokens_bounds($text) };
+
+    for my $pos (keys %$ethalon) {
+        if(exists $bounds{$pos}) { $stats{tp}++ }
+        else { $stats{fn}++ }
+    }
+    for my $pos (keys %bounds) {
+        $stats{fp}++ unless exists $ethalon->{$pos};
+    }
+}
+
+my $precision = eval { $stats{tp} / ($stats{tp} + $stats{fp}) } || 0;
+my $recall    = eval { $stats{tp} / ($stats{tp} + $stats{fn}) } || 0;
 printf "Threshold: %s, Precision: %.4f, Recall: %.4f, F1: %.4f\n",
     $opts{threshold},
     $precision,
     $recall,
-    F_score(1, $precision, $recall);
+    F_measure(1, $precision, $recall);
 
-sub F_score {
+sub F_measure {
     my($B, $P, $R) = @_;
 
     ((1 + $B ** 2) * ($P * $R)) / ($B ** 2 * $P + $R)
 }
 
-sub all_occurences {
-    my($str, $substr) = @_;
+sub get_tokens {
+    my $id = shift;
 
-    my @occurences;
-    my $offset = 0;
-    while($offset < length $str) {
-        my $pos = index $str, $substr, $offset;
-        last if $pos < 0;
+    my $sth = $dbh->prepare_cached("
+        select
+            tf_text
+        from
+            text_forms
+        WHERE
+            sent_id = ?
+        order by
+            pos
+    ", undef, 3);
 
-        push @occurences, $pos + 1;
-        $offset = $pos + 1;
+    $dbh->selectcol_arrayref($sth, undef, $id);
+}
+
+sub get_bounds {
+    my($text, $tokens) = @_;
+
+    my %bounds;
+    my $pos = 0;
+    for my $token (@$tokens) {
+        while(substr($text, $pos, length $token) ne $token) {
+            $pos++;
+
+            die "Weird token [$token] in sentence [$text]"
+                if $pos > length $text;
+        }
+        $bounds{$pos + length($token)-1} = undef;
+        $pos += length $token;
     }
 
-    \@occurences;
+    return \%bounds;
 }
 
 sub usage { pod2usage({-verbose => $_[0]}) }
@@ -114,7 +136,7 @@ perl calculate_metrics.pl --options
 
 QA tool for Perl tokenizer.
 
-Calculates IR-related metrics for what Perl tokenizer produces. This includes precision, recall and F-score.
+Calculates IR-related metrics for what Perl tokenizer produces. This includes precision, recall and F-measure.
 
 =head1 OPTIONS
 
@@ -137,6 +159,12 @@ Tokenizer's threshold.
 Optional.
 
 Path to tokenizer's data directory. Defaults to distribution directory.
+
+=item --skip
+
+Optional.
+
+Path to a list of sentences to skip.
 
 =item --help
 
