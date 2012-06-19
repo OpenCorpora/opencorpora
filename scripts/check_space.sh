3864b657b4638317a964fa013dff2a428b4b49af
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-06-19 11:50:24 +0000

    sending alert emails about too little space earlier

--- scripts/check_space.sh
+++ scripts/check_space.sh
@@ -3,11 +3,11 @@ if [ -f /var/lock/oc_readonly.lock ]; then
     exit
 fi
 
-if [ `df | grep sql_opencorpora | awk '{print $4}'` -lt 100000 ]; then
+if [ `df | grep sql_opencorpora | awk '{print $4}'` -lt 500000 ]; then
     touch /var/lock/oc_readonly.lock
     echo 'Insufficient space on opencorpora.org!' | sendmail dima.granovsky@gmail.com
 fi
-if [ `df | grep dat_opencorpora | awk '{print $4}'` -lt 100000 ]; then
+if [ `df | grep dat_opencorpora | awk '{print $4}'` -lt 300000 ]; then
     touch /var/lock/oc_readonly.lock
     echo 'Insufficient space on opencorpora.org!' | sendmail dima.granovsky@gmail.com
 fi
