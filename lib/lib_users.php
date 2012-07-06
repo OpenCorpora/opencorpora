a115359710e2e5a387f4b95fc6bd2cd8458acf0c
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-07-06 10:41:42 +0000

    allow dot in shown name as well

--- lib/lib_users.php
+++ lib/lib_users.php
@@ -200,7 +200,7 @@ function user_change_email($post) {
 }
 function user_change_shown_name($new_name) {
     $new_name = trim($new_name);
-    if (!preg_match('/^[a-zа-я0-9_- ]{2,}$/ui', $new_name))
+    if (!preg_match('/^[a-zа-я0-9ё_- .]{2,}$/ui', $new_name))
         return 2;
     if (!sql_query("UPDATE users SET user_shown_name = '".mysql_real_escape_string($new_name)."' WHERE user_id = ".$_SESSION['user_id']." LIMIT 1"))
         return 0;
