e83c332a53e2ce18529904f007a2ad3485d93ef4
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-09-16 11:24:42 +0000

    better check level

--- lib/lib_awards.php
+++ lib/lib_awards.php
@@ -69,6 +69,8 @@ function get_user_level($user_id) {
     return $r['user_level'];
 }
 function check_user_level($user_id) {
+    if (!$user_id)
+        return 0;
     $r = sql_fetch_array(sql_query("SELECT user_rating10, user_level, user_shown_level FROM users WHERE user_id=$user_id LIMIT 1"));
     $next_level = $r['user_level'];
     $last_shown_level = $r['user_shown_level'];
