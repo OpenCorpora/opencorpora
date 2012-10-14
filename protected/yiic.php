95c04deab0679141b25ea33b32f700aa77d6d6b0
Maria Nikolaeva <mary.nikolaeva@gmail.com>
2012-10-14 12:59:57 +0000

    yii app: started migrations

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,13 @@
 <?php
 
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
+// set environment
+require_once(dirname(__FILE__) . '/extensions/yii-environment/Environment.php');
+$env = new Environment(null, true); //determine mode by file
 
-require_once($yiic);
+// run Yii app
+$config = $env->configConsole;
+require_once($env->yiicPath);
+
+// Want to execute runYiiStatics()? Modify yiic.php
+// AFTER: require_once(dirname(__FILE__).'/yii.php');
+// ADD: $env->runYiiStatics();
