40a312cabfefc4a50af07dd3f2ebe0d192d013ec
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-10-14 09:09:17 +0000

    pool visibility doesn't affect tab-separated

--- pools.php
+++ pools.php
@@ -11,7 +11,7 @@ if ($action && !in_array($action, array('samples', 'candidates')) && !user_has_p
 }
 
 // temporarily hidden pools
-if (isset($_GET['pool_id']) && in_array((int)$_GET['pool_id'], array(19, 23, 29, 46))) {
+if (isset($_GET['pool_id']) && !isset($_GET['tabs']) && in_array((int)$_GET['pool_id'], array(19, 23, 29, 46))) {
     show_error("Этот пул временно скрыт.");
     return;
 }
