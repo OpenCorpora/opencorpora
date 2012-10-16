922d8d3883e02d2ffe4973dc5162365a4ecc1bf8
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-10-16 10:13:43 +0000

    we don't need no captcha

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -6,11 +6,6 @@ class SiteController extends Controller {
      */
     public function actions() {
         return array(
-            // captcha action renders the CAPTCHA image displayed on the contact page
-            'captcha'=>array(
-                'class'=>'CCaptchaAction',
-                'backColor'=>0xFFFFFF,
-            ),
             // page action renders "static" pages stored under 'protected/views/site/pages'
             // They can be accessed via: index.php?r=site/page&view=FileName
             'page'=>array(
