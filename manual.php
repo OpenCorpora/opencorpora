105de50ea03068cdb059f856e60e62b69547bc49
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-10-18 16:46:40 +0000

    generic manual shown if no type given

--- manual.php
+++ manual.php
@@ -2,11 +2,12 @@
 require('lib/header.php');
 require_once('lib/lib_annot.php');
 
-if (!isset($_GET['pool_type'])) {
-    show_error();
-    return;
-}
+$pool_type = isset($_GET['pool_type']) ? (int)$_GET['pool_type'] : 0;
 
-$smarty->assign('content', get_wiki_page(get_pool_manual_page((int)$_GET['pool_type'])));
+if ($pool_type)
+    $smarty->assign('content', get_wiki_page(get_pool_manual_page($pool_type)));
+else
+    $smarty->assign('content', get_wiki_page("Инструкция по интерфейсу для снятия омонимии"));
+
 $smarty->display('static/doc/annotation.tpl');
 ?>
