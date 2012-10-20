a3f5310a18be95102c13b9803c62e65ef1771e12
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-10-20 08:35:43 +0000

    stats scripts moved to a separate folder

--- scripts/exports.sh
+++ scripts/exports.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-/corpus/scripts/export_annot.sh
-/corpus/scripts/export_ngram.sh
-/corpus/scripts/export_dict.sh
+/corpus/scripts/export/export_annot.sh
+/corpus/scripts/export/export_ngram.sh
+/corpus/scripts/export/export_dict.sh
--- scripts/update_stats.sh
+++
@@ -1,2 +0,0 @@
-/corpus/scripts/cronrunner.pl "/corpus/scripts/update_stats.pl /corpus/config.ini"
-/corpus/scripts/cronrunner.pl "/corpus/scripts/update_tag_stats.pl /corpus/config.ini"
