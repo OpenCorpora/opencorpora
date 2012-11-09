ceaf6c27b194358d7cdfb599e1fc761214dd2df4
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-11-09 11:32:18 +0000

    recognize numbers as NUMB

--- lib/lib_dict.php
+++ lib/lib_dict.php
@@ -57,6 +57,8 @@ function generate_tf_rev($token) {
         }
     } elseif (preg_match('/^\p{P}+$/u', $token)) {
         $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="PNCT"/></l></v>';
+    } elseif (preg_match('/^[\.,\p{Nd}]+$/u', $token)) {
+        $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="NUMB"/></l></v>';
     } else {
         $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="UNKN"/></l></v>';
     }
