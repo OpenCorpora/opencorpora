8c32eb75f3ac8a01daf3f157e4ab6182f2599271
Victor Bocharov <victor.bocharov@gmail.com>
2012-11-15 23:50:17 +0000

    pool list added

--- export/pools/export_pools.sh
+++ export/pools/export_pools.sh
@@ -1,8 +1,10 @@
 mkdir tmp
 
-for id in $( perl ../pools.pl /corpus/config.ini | grep -E "[45679]$" | gawk '{ print $1 }' )
+perl ../pools.pl /corpus/config.ini | grep -E "[45679]$" > tmp/pools.txt
+
+for id in $( cat tmp/pools.txt | gawk '{ print $1 }' )
 do
-  echo "id = $id"
+#  echo "id = $id"
   wget "http://opencorpora.org/pools.php?act=samples&pool_id=$id&tabs&mod_ans" --output-document=tmp/pool_$id.tab 
 done
 
@@ -10,3 +12,5 @@ cd tmp
 zip -9 ../pools.zip pool*.tab
 tar -cvjf ../pools.tar.bz2 pool*.tab --remove-files
 cd ..
+
+rmdir tmp
