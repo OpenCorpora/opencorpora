e16a4ef7605032211fb45630c4d1f3135674117c
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-11-19 07:20:47 +0000

    recognize roman numbers

--- lib/lib_dict.php
+++ lib/lib_dict.php
@@ -59,8 +59,10 @@ function generate_tf_rev($token) {
         $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="PNCT"/></l></v>';
     } elseif (preg_match('/^\p{Nd}+[\.,]?\p{Nd}*$/u', $token)) {
         $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="NUMB"/></l></v>';
-    } elseif (preg_match('/^[\p{Latin}\.-]+$/u', $token) && !preg_match('/^[IVXLCM]+$/u', $token)) {
+    } elseif (preg_match('/^[\p{Latin}\.-]+$/u', $token)) {
         $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="LATN"/></l></v>';
+        if (preg_match('/^[IVXLCMDivxlcmd]+$/u', $token))
+            $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="ROMN"/></l></v>';
     } else {
         $out .= '<v><l id="0" t="'.htmlspecialchars($token).'"><g v="UNKN"/></l></v>';
     }
