a41421c89cd4adcbf94444b16171735c51ab64dd
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-12-04 11:31:08 +0000

    fixed wrong paths

--- export/pools/export_pools.sh
+++ export/pools/export_pools.sh
@@ -7,11 +7,11 @@ if [ ! -e $TMPDIR ]; then
     mkdir $TMPDIR
 fi
 
-perl ../pools.pl /corpus/config.ini | grep -E "[45679]$" > tmp/pools.txt
+perl ../pools.pl /corpus/config.ini | grep -E "[45679]$" > $TMPDIR/pools.txt
 
-for id in $( cat tmp/pools.txt | gawk '{ print $1 }' )
+for id in $( cat $TMPDIR/pools.txt | gawk '{ print $1 }' )
 do
-  wget -q "http://localhost/pools.php?act=samples&pool_id=$id&tabs&mod_ans" --output-document=tmp/pool_$id.tab 
+  wget -q "http://localhost/pools.php?act=samples&pool_id=$id&tabs&mod_ans" --output-document=$TMPDIR/pool_$id.tab 
 done
 
 cd $TMPDIR
