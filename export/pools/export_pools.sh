94c0393c74f5975649d2a61216f849ff04bcc8e2
Dmitry Granovsky <dima.granovsky@gmail.com>
2012-12-20 09:36:31 +0000

    fixed pool export script

--- export/pools/export_pools.sh
+++ export/pools/export_pools.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+WORK_DIR=`dirname $0`
 
-TMPDIR=`dirname $0`/tmp
+TMPDIR=$WORK_DIR/tmp
 EXPORT_DIR=/corpus/files/export/pools
 
 if [ ! -e $TMPDIR ]; then
     mkdir $TMPDIR
 fi
 
-perl ../pools.pl /corpus/config.ini | grep -E "[45679]$" > $TMPDIR/pools.txt
+perl $WORK_DIR/../pools.pl /corpus/config.ini | grep -E "[45679]$" > $TMPDIR/pools.txt
 
 for id in $( cat $TMPDIR/pools.txt | gawk '{ print $1 }' )
 do
@@ -17,6 +18,5 @@ done
 cd $TMPDIR
 zip -q9 $EXPORT_DIR/pools.zip pool*.t*
 tar -cjf $EXPORT_DIR/pools.tar.bz2 pool*.tab pools.txt --remove-files
-cd ..
 
 rm -rf $TMPDIR
