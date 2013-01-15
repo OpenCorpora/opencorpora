40d2aa1fa0aecb5715ab8b3af7d6e94656de9de9
Victor Bocharov <victor.bocharov@gmail.com>
2013-01-15 14:48:42 +0000

    bug

--- postagging/brill/unsupervised/cpp/train/main.cpp
+++ postagging/brill/unsupervised/cpp/train/main.cpp
@@ -565,7 +565,7 @@ float DoOneStep(SentenceCollection &sc, map<TagSet, TagStat> &tStat, list<Rule> 
     
       cerr << r.str() << endl;
       knownRules.push_back(r);
-      break; // временно отключаем повторы
+      //break; // временно отключаем повторы
 
 /*      if (tStat[bestRules[i].from].freq > maxTSFreq) {
         nBestRule = i;
@@ -616,7 +616,7 @@ size_t ApplyRule(SentenceCollection &sc, const Rule &rule, map<TagSet, TagStat> 
   for (size_t i = 0; i < r.idx.size(); ++i) {
     Sentence &rs = sc[r.idx[i].first];
 
-    if (rule.c.match(rs, r.idx[i].second && rs.getToken(r.idx[i].second).getPOST() == rule.from)) {
+    if (rule.c.match(rs, r.idx[i].second) && rs.getToken(r.idx[i].second).getPOST() == rule.from) {
       // вторая проверка нужна на случай, если это не первое правило в группе и предыдущие
       // уже изменили этот токен ... может быть стоит отмечать такие токены в индексе?
       rs.getNonConstToken(r.idx[i].second).deleteAllButThis(rule.to);
