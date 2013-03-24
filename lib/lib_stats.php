576f22c50eb5b3402c05cc2a4dbe10dc60980255
Dmitry Granovsky <dima.granovsky@gmail.com>
2013-03-24 20:35:59 +0000

    revert r1759: incorrect numbers

--- lib/lib_stats.php
+++ lib/lib_stats.php
@@ -391,7 +391,7 @@ function get_moderation_stats() {
     foreach ($t as $mod => $mdata) {
         foreach ($mdata as $type => $tdata) {
             foreach ($tdata as $st => $sdata) {
-                if (isset($mod_total[$mod]) && isset($mod_total[$mod][$type])) {
+                if ($sdata > 0) {
                     $t[$mod][$type][$st] = array($sdata, intval($sdata / $mod_total[$mod][$type] * 100));
                     $t[$mod]['total'][$st][0] += $sdata;
                     $t[$mod]['total'][$st][1] += $sdata / $mod_total[$mod]['total'] * 100;
