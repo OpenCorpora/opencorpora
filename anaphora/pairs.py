3fd453617ecac5c942cccf2bb92f495612b52010
Svetlana Volskaya <svetlana.volskaya@gmail.com>
2014-03-05 19:14:56 +0000

    generating pairs for learning

--- anaphora/pairs.py
+++ anaphora/pairs.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+ 
 import sys
 import argparse
 import itertools
-     
+ 
 parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description='Make pairs "group id - pronoun id".')
 parser.add_argument('infile1', nargs='?', type = argparse.FileType('r'), help = 'file with a list of groups')
 parser.add_argument('infile2', nargs='?', type = argparse.FileType('r'), help = 'file with a list of pronouns')
 args = parser.parse_args()
      
+     
+def getid(fullid):
+    return int(fullid.split('_')[1])
+ 
+     
+def gettextid(fullid):
+    return int(fullid.split('_')[0])
      
 groups = {}
 pron = {}
@@ -17,16 +24,16 @@ pron = {}
      
 for s in args.infile1:                 # dict group_id : max token
     if not s.rstrip('\r\n'):
-        continue                    
+        continue
     s = s.strip().split('\t')
-    groups[int(s[0])] = int(max(s[1].split(',')))
+    groups[s[0]] = s[1].split(',')[-1]
  
  
 for line in args.infile2:              # dict pron_id : token number
     if not line.rstrip('\r\n'):
         continue
     line = line.strip().split('\t')
-    pron[int(line[0])] = int(line[1])
+    pron[line[0]] = line[1]
  
  
 group_keys = sorted(groups.keys())
@@ -34,9 +41,15 @@ pronoun_keys = sorted(pron.keys())
  
  
 result = itertools.product(group_keys, pronoun_keys)
+prev = 0
 for i in result:
-    if groups[i[0]] < pron[i[1]]:
+    g = getid(groups[i[0]])
+    tg = gettextid(groups[i[0]])
+    tp = gettextid(pron[i[1]])
+    if tg == tp and g < getid(pron[i[1]]) \
+        and g > prev:
         sys.stdout.write('{0}__{1}'.format(str(i[0]), str(i[1]) + '\n'))
-    if groups[i[0]] > pron[i[1]]:
+        prev = getid(pron[i[1]])
+    else:
         continue
 
