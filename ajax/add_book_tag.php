2b1d88a7797825e5b3014fedbfac89b7bfe772f8
Dmitry Granovsky <dima.granovsky@gmail.com>
2014-04-04 17:20:57 +0400

    syntax error fix

--- ajax/add_book_tag.php
+++ ajax/add_book_tag.php
@@ -3,8 +3,9 @@ require_once('../lib/header_ajax.php');
 require_once('../lib/lib_books.php');
 $result = 1;
 
-try
+try {
     books_add_tag((int)$_GET['book_id'], mysql_real_escape_string($_GET['tag_name']));
+}
 catch (Exception $e) {
     $result = 0;
 }
