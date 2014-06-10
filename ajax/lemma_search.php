f309000c06808e35c12c94b08cf684ff38ce81e6
Dmitry Granovsky <dima.granovsky@gmail.com>
2014-06-10 18:58:26 +0400

    syntax fix

--- ajax/lemma_search.php
+++ ajax/lemma_search.php
@@ -3,7 +3,7 @@ require_once('../lib/header_ajax.php');
 require_once('../lib/lib_dict.php');
 echo '<?xml version="1.0" encoding="utf-8" standalone="yes"?><lemmata>';
 $res = sql_pe("SELECT lemma_id FROM dict_lemmata WHERE lemma_text=?", array($_GET['q']));
-foreach ($res as $r)
+foreach ($res as $r) {
     echo '<lemma id="'.$r['lemma_id'].'"/>';
 }
 echo '</lemmata>';
