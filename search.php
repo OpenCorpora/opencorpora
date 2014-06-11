81eda28d213c10664c06311853417c813f899080
Dmitry Granovsky <dima.granovsky@gmail.com>
2014-06-11 17:55:47 +0400

    search by all forms by default (via interface not api)

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ require('lib/lib_annot.php');
 
 if (isset($_GET['q'])) {
     $search = trim(mb_strtolower($_GET['q']));
-    $smarty->assign('search', get_search_results($search));
+    $smarty->assign('search', get_search_results($search, isset($_GET['exact_form'])));
     $smarty->display('search.tpl');
 } else
     show_error("Не задан поисковый запрос");
