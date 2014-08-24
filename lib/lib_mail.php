a8b84d09df0b8e4964c813081266428811b6265c
Alex Chuchunkov <scarywound@gmail.com>
2014-08-24 17:27:54 +0400

    switched to composer, changed send_email

--- lib/lib_mail.php
+++ lib/lib_mail.php
@@ -1,19 +1,21 @@
 <?php
-require_once('Mail.php');
+
 function send_email($to, $header, $body) {
     //$to may be either an array or a comma-separated string
-
-    $mail =& Mail::factory('mail');
-
-    mb_internal_encoding('utf-8');
-
-    $headers['From'] = 'OpenCorpora <robot@opencorpora.org>';
-    $headers['To'] = $to;
-    $headers['Subject'] = mb_encode_mimeheader($header, 'utf-8');
-    $headers['Content-Type'] = 'text/plain; charset="UTF-8"';
-    $headers['Content-Transfer-Encoding'] = '8bit';
-
-    $b = $mail->send($to, $headers, $body);
-    return $b;
+    if (!is_array($to)) $to = explode(',', $to);
+    // Create the message
+    $message = Swift_Message::newInstance()
+      // Give the message a subject
+      ->setSubject($header)
+      // Set the From address with an associative array
+      ->setFrom(array('robot@opencorpora.org' => 'OpenCorpora'))
+      // Set the To addresses with an associative array
+      ->setTo($to)
+      // Give it a body
+      ->setBody($body);
+    // Create the Transport
+    $transport = Swift_MailTransport::newInstance(); // should switch to SMTP though!
+    // Create the Mailer using your created Transport
+    $mailer = Swift_Mailer::newInstance($transport);
+    return $mailer->send($message);
 }
-?>
