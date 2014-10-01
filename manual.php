b27259629af4a2421f4acb4013f2963a51cc0e71
Dmitry Granovsky <dima.granovsky@gmail.com>
2014-10-01 11:27:10 +0400

    show link to moderation manual on mainpage for moderators, closes #112

--- manual.php
+++ manual.php
@@ -3,11 +3,18 @@ require('lib/header.php');
 require_once('lib/lib_annot.php');
 
 $pool_type = isset($_GET['pool_type']) ? $_GET['pool_type'] : 0;
+$what = isset($_GET['what']) ? $_GET['what'] : '';
 
-if ($pool_type)
-    $smarty->assign('content', get_wiki_page(get_pool_manual_page($pool_type)));
-else
-    $smarty->assign('content', get_wiki_page("Инструкция по интерфейсу для снятия омонимии"));
+switch ($what) {
+    case 'morph_moderation':
+        $smarty->assign('content', get_wiki_page("Инструкция для модераторов"));
+        break;
+    default:
+        if ($pool_type)
+            $smarty->assign('content', get_wiki_page(get_pool_manual_page($pool_type)));
+        else
+            $smarty->assign('content', get_wiki_page("Инструкция по интерфейсу для снятия омонимии"));
+}
 
 $smarty->display('static/doc/annotation.tpl');
 log_timing();
