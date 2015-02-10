7bd75e0debfdb721440432de28f9d4ad43195d2f
Dmitry Granovsky <dima.granovsky@gmail.com>
2015-02-10 20:36:31 +0300

    turn back on tag stats updates

--- scripts/stats/update_stats.sh
+++ scripts/stats/update_stats.sh
@@ -1,2 +1,2 @@
 /corpus/scripts/cronrunner.pl "/corpus/scripts/stats/update_stats.pl /corpus/config.ini"
-#/corpus/scripts/cronrunner.pl "/corpus/scripts/stats/update_tag_stats.pl /corpus/config.ini"
+/corpus/scripts/cronrunner.pl "/corpus/scripts/stats/update_tag_stats.pl /corpus/config.ini"
