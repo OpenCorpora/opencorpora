3d22e737f3d9deea523a5af6d719fc6148a0a3d0
Alex Chuchunkov <scarywound@gmail.com>
2015-05-02 01:04:58 +0300

    dog achievement shall be checked at the beginning of the month

--- scripts/check_dog_achievement.php
+++ scripts/check_dog_achievement.php
@@ -6,14 +6,15 @@ if (php_sapi_name() == 'cli') {
     require_once("lib/header.php");
     require_once("lib/lib_achievements.php");
 
+    if (date("j") != 1) die("Today is not the first day of the month, finishing.\n");
+
     print "Pinging dog achievement recalculation.. \n";
     // get all users which registered the same day as today
     $users = sql_pe("SELECT user_id FROM users
-    	WHERE DAY(FROM_UNIXTIME(user_reg)) = DAY(NOW())
-    		AND DATE(FROM_UNIXTIME(user_reg)) != DATE(NOW())
+    	WHERE DATE(FROM_UNIXTIME(user_reg)) < DATE_SUB(DATE(NOW()), INTERVAL 15 DAY)
     	   ", array());
 
-    print "Month(s) since registration passed for ".count($users)." user(s)\n";
+    print "Month(s) (or at least 15 days) since registration passed for ".count($users)." user(s)\n";
     foreach ($users as $u) {
     	$am = new AchievementsManager((int)$u['user_id']);
     	$am->emit(EventTypes::MONTH_PASSED);
