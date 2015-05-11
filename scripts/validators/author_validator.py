1e35e9ce8fb3ddb8fe51a6a517e3b5b0125809fa
Victor Bocharov <victor.bocharov@gmail.com>
2015-05-11 22:47:19 +0300

    Учёт наследования тегов в валидаторе тега Автор:

--- scripts/validators/author_validator.py
+++ scripts/validators/author_validator.py
@@ -15,26 +15,76 @@ db = MySQLdb.connect(hostname, username, password, dbname)
 cursor = db.cursor()
 cursor.execute('SET NAMES utf8')
 cursor.execute("""DELETE FROM tag_errors WHERE error_type = 3""")
-cursor.execute("""SELECT book_id  FROM books 
-		  WHERE book_id NOT IN 
-			(SELECT book_id FROM book_tags WHERE tag_name LIKE 'Автор:%') 
-		  AND parent_id NOT IN (8, 226)
-		  AND parent_id NOT IN 
-			(SELECT book_id FROM books WHERE parent_id = 184) 
-		  AND parent_id NOT IN 
-			(SELECT book_id FROM books WHERE parent_id IN 
-				(SELECT book_id FROM books WHERE parent_id IN 
-					(SELECT book_id FROM books WHERE parent_id = 806))) 
-		  AND parent_id NOT IN 
-			(SELECT book_id FROM books WHERE parent_id IN 
-				(SELECT book_id FROM books WHERE parent_id = 806)) 
-		  AND parent_id NOT IN 
-			(SELECT book_id FROM books WHERE parent_id = 806)
-		  AND parent_id > 0""")
+cursor.execute("""SELECT book_id  FROM books
+                  WHERE book_id NOT IN (459, 2171, 2186, 2270, 2273, 2284, 3374, 3384, 3395, 3463)
+                  AND book_id NOT IN
+                      (SELECT book_id FROM book_tags WHERE tag_name LIKE 'Автор:%')
+                  AND book_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM book_tags WHERE tag_name LIKE 'Автор:%'))
+                  AND book_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM book_tags WHERE tag_name LIKE 'Автор:%')))
+                  AND book_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN
+                                  (SELECT book_id FROM book_tags WHERE tag_name LIKE 'Автор:%'))))
+                  AND book_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN
+                                  (SELECT book_id FROM books WHERE parent_id IN
+                                      (SELECT book_id FROM book_tags WHERE tag_name LIKE 'Автор:%')))))
+                  AND book_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN
+                                  (SELECT book_id FROM books WHERE parent_id IN
+                                      (SELECT book_id FROM books WHERE parent_id IN
+                                          (SELECT book_id FROM book_tags WHERE tag_name LIKE 'Автор:%'))))))
+                  AND parent_id NOT IN (8, 226, 1675, 2171, 2186, 2270, 2273, 2284)
+                  AND parent_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN
+                                  (SELECT book_id FROM books WHERE parent_id IN
+                                      (SELECT book_id FROM books WHERE parent_id IN
+                                          (SELECT book_id FROM books WHERE parent_id IN
+                                              (SELECT book_id FROM books WHERE parent_id IN (806, 1675, 2186))))))))
+                  AND parent_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN
+                                  (SELECT book_id FROM books WHERE parent_id IN
+                                      (SELECT book_id FROM books WHERE parent_id IN
+                                          (SELECT book_id FROM books WHERE parent_id IN (806, 1675, 2186)))))))
+                  AND parent_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN
+                                  (SELECT book_id FROM books WHERE parent_id IN
+                                      (SELECT book_id FROM books WHERE parent_id IN (806, 1675, 2186))))))
+                  AND parent_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN
+                                  (SELECT book_id FROM books WHERE parent_id IN (806, 1675, 2186)))))
+                  AND parent_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN
+                              (SELECT book_id FROM books WHERE parent_id IN (806, 1675, 2186))))
+                  AND parent_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN
+                          (SELECT book_id FROM books WHERE parent_id IN (806, 1675, 2186)))
+                  AND parent_id NOT IN
+                      (SELECT book_id FROM books WHERE parent_id IN (184, 806, 1675, 2186, 2284, 3384))
+                  AND parent_id > 0""")
 data = cursor.fetchall();
 for i in data:
-	query = """INSERT INTO tag_errors VALUES(%d, '%s', %d)""" % (i[0], '', 3)
-	cursor.execute(query)
+    query = """INSERT INTO tag_errors VALUES(%d, '%s', %d)""" % (i[0], '', 3)
+    cursor.execute(query)
 db.commit()
 db.close
 
