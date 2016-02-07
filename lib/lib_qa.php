0fbb60e306a854c677444d0454ca1664ee2d1b55
Dmitry Granovsky <dima.granovsky@gmail.com>
2016-02-08 02:04:18 +0300

    show non-checked first on merge fails page

--- lib/lib_qa.php
+++ lib/lib_qa.php
@@ -129,7 +129,7 @@ function get_merge_fails() {
         LEFT JOIN morph_annot_merge_comments c USING (sample_id)
         WHERE p.status = ".MA_POOLS_STATUS_ARCHIVED."
         AND merge_status in (0, 2)
-        ORDER BY sample_id
+        ORDER BY merge_status, sample_id
     ");
             
     $res1 = sql_prepare("
