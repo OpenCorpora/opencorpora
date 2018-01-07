fff094376c0123dc39a1b0c000df0d69b8f9e004
Dmitry Granovsky <dima.granovsky@gmail.com>
2018-01-07 12:00:06 +0300

    [tokenizer] add reading limit in train.php

--- scripts/tokenizer/train.php
+++ scripts/tokenizer/train.php
@@ -7,5 +7,9 @@ set_include_path(get_include_path().PATH_SEPARATOR.'/corpus');
 require_once('lib/header_ajax.php');
 require_once('lib/lib_tokenizer.php');
 
+$limit = 0;
+if ($argc == 2)
+    $limit = intval($argv[1]);
+
 $tokenizer = new Tokenizer(__DIR__);
-$tokenizer->train();
+$tokenizer->train($limit);
