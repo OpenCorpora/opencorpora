705a4336a0e43099e841cc57eac4a3b9b0e79311
Dmitry Granovsky <dima.granovsky@gmail.com>
2019-04-30 21:53:26 +0300

    better importer

--- scripts/aot_import/importer.py
+++ scripts/aot_import/importer.py
@@ -22,23 +22,25 @@ def do_import(editor, stream):
             form_text, gram = line.split('\t')
             form_text = form_text.lower().encode('utf-8')
             assert all(c in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя-' for c in form_text)
-            gram_lemma, gram_form = gram.encode('utf-8').split(' ')
+            gram_parts = gram.encode('utf-8').split(' ')
+            gram_lemma = gram_parts[0].split(',')
+            gram_form = gram_parts[1].split(',') if len(gram_parts) == 2 else []
             if not in_lemma:
                 if new_lemma is not None:
                     new_lemma.save()
                     new_lemma = None
-                found = editor.find_lexeme_by_lemma(form_text, ['NOUN'])
+                found = editor.find_lexeme_by_lemma(form_text, ['ADVB'])
                 if len(found) > 0:
                     print("found {} {}: {}".format(len(found), form_text, ','.join(found[0].lemma['gram'])))
                     skip = True
                 else:
                     new_lemma = Lexeme(lemma=form_text, editor=editor)
-                    new_lemma.add_lemma_gram(gram_lemma.split(','))
-                    new_lemma.add_form(form_text, gram_form.split(','))
+                    new_lemma.add_lemma_gram(gram_lemma)
+                    new_lemma.add_form(form_text, gram_form)
                     skip = False
                 in_lemma = True
             elif not skip:
-                new_lemma.add_form(form_text, gram_form.split(','))
+                new_lemma.add_form(form_text, gram_form)
 
 
 def main():
