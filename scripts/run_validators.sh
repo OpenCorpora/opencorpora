21c9f4fb0e1374fe3abc0aee2f5467f9b52b109b
Dmitry Granovsky <dima.granovsky@gmail.com>
2020-07-13 10:00:02 +0300

    fix running fing_good_sentences

--- scripts/run_validators.sh
+++ scripts/run_validators.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-config=/corpus/config.ini
+CONFIG_INI=/corpus/config.ini
+CONFIG_JSON=/corpus/config.json
 
-python /corpus/scripts/validators/year_valid.py $config
-python /corpus/scripts/validators/author_validator.py $config
-python /corpus/scripts/validators/url_validator.py $config
-python /corpus/scripts/validators/par_validator.py $config
-/corpus/scripts/find_good_sentences.py $config
+python /corpus/scripts/validators/year_valid.py $CONFIG_INI
+python /corpus/scripts/validators/author_validator.py $CONFIG_INI
+python /corpus/scripts/validators/url_validator.py $CONFIG_INI
+python /corpus/scripts/validators/par_validator.py $CONFIG_INI
+/corpus/scripts/find_good_sentences.py $CONFIG_JSON
